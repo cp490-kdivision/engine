074aa5d1ddff70a963dd2e7810f609f0aa0d6956
stran-minh <sonkut1234@gmail.com>
2021-04-14 17:38:39 -0400

    updated event post functions

--- SendData.py
+++ SendData.py
@@ -11,10 +11,9 @@ cnxn: pyodbc.Connection = pyodbc.connect(coalEngineDBCon.con)
 #cursor object from connection
 crsr: pyodbc.Cursor = cnxn.cursor()
 
-def postEvent(eventJson):
+def postEvent(eventJson,eventID):
     try:
         #inject Events
-        eventID = IDgen.generateID('event_ID','Events','')
         eventName = eventJson['command']
         # eventCondition = json.dumps(eventJson['conditions'])
         eventCondition = json.dumps(eventJson['conditions'])
@@ -22,24 +21,8 @@ def postEvent(eventJson):
         #execute injection
         crsr.execute(eventSQL)
         crsr.commit()
-
-        #inject event true
-        trueID = IDgen.generateID('true_ID','Events_True',eventID)
-        eventTrue = json.dumps(eventJson['true_part'])
-        trueSQL = "INSERT INTO [Events_True] (true_ID,event_ID,true_Argument) VALUES ('{}','{}','{}')".format(trueID,eventID,eventTrue)
-        #execute injection
-        crsr.execute(trueSQL)
-        crsr.commit()
-
-        #inject event false
-        falseID = IDgen.generateID('false_ID','Events_False',eventID)
-        eventFalse = json.dumps(eventJson['false_part'])
-        falseSQL = "INSERT INTO [Events_False] (false_ID,event_ID,false_Argument) VALUES ('{}','{}','{}')".format(falseID,eventID,eventFalse)
-        # #execute injection
-        crsr.execute(falseSQL)
-        crsr.commit()
-        cnxn.close()
-        return 'Injection success' 
+        return 'Injection success'
+    #error handler
     except pyodbc.Error as err:
         print(err.args[1])
         cnxn.close()
@@ -48,5 +31,41 @@ def postEvent(eventJson):
         cnxn.close()
         return 'Error 200: code error'
 
-respond = postEvent(data)
-print(respond)
+def postTrue(trueJson,eventID):
+    try:
+        #inject event true
+        trueID = IDgen.generateID('true_ID','Events_True',eventID)
+        eventTrue = json.dumps(trueJson['true_part'])
+        trueSQL = "INSERT INTO [Events_True] (true_ID,event_ID,true_Argument) VALUES ('{}','{}','{}')".format(trueID,eventID,eventTrue)
+        #execute injection
+        crsr.execute(trueSQL)
+        crsr.commit()
+        return 'Injection success'
+    #error handler
+    except pyodbc.Error as err:
+        print(err.args[1])
+        cnxn.close()
+        return 'Error 400: data injection failed'
+    except:
+        cnxn.close()
+        return 'Error 200: code error'
+    
+def postFalse(falseJson,eventID):
+    try:
+        #inject event false
+        falseID = IDgen.generateID('false_ID','Events_False',eventID)
+        eventFalse = json.dumps(falseJson['false_part'])
+        falseSQL = "INSERT INTO [Events_False] (false_ID,event_ID,false_Argument) VALUES ('{}','{}','{}')".format(falseID,eventID,eventFalse)
+        # #execute injection
+        crsr.execute(falseSQL)
+        crsr.commit()
+        cnxn.close()
+        return 'Injection success'
+    #error handler
+    except pyodbc.Error as err:
+        print(err.args[1])
+        cnxn.close()
+        return 'Error 400: data injection failed'
+    except:
+        cnxn.close()
+        return 'Error 200: code error'
